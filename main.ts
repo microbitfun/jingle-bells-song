6110fdec9d87e097fbd979bbd57a6da27ea69f7f
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-02 14:42:48 +0200

    initial version

--- main.ts
+++ main.ts
@@ -10,5 +10,5 @@ input.onButtonPressed(Button.A, function () {
 })
 for (let index = 0; index < 4; index++) {
     basic.showIcon(IconNames.Diamond)
-    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Heart)
 }
